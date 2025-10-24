f6637e0b5e6919df4a818225a3516bc8ea87da68
nagitodst <ecaros14@gmail.com>
2025-10-24 09:15:00 -0300

    slide 43 do modulo 9

--- app/src/main/java/com/alex/task/ui/SplashFragment.kt
+++ app/src/main/java/com/alex/task/ui/SplashFragment.kt
@@ -7,15 +7,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.alex.task.R
 import com.alex.task.databinding.FragmentSplashBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class SplashFragment : Fragment() {
 
     private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,7 +36,18 @@ class SplashFragment : Fragment() {
     }
 
     private fun checkAuth(){
-        findNavController().navigate(R.id.action_splashFragment_to_autentication)
+        try {
+            val currentUser = auth.currentUser
+
+            if (currentUser != null) {
+                findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+            }else{
+                findNavController().navigate(R.id.action_splashFragment_to_autentication)
+            }
+        }catch (e: Exception) {
+            Toast.makeText(requireContext(),e.message.toString(),Toast.LENGTH_SHORT)
+            findNavController().navigate(R.id.action_splashFragment_to_autentication)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/alex/task/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/alex/task/ui/auth/RegisterFragment.kt
@@ -5,16 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.alex.task.R
 import com.alex.task.databinding.FragmentRegisterBinding
 import com.alex.task.util.initToolbar
 import com.alex.task.util.showBottomSheet
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterFragment : Fragment() {
 
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,12 +48,32 @@ class RegisterFragment : Fragment() {
         val senha = binding.editTextSenha.text.toString().trim()
         if(email.isNotBlank()) {
             if (senha.isNotBlank()) {
-                findNavController().navigate(R.id.action_global_homeFragment)
+                binding.progressbar.isVisible = true
+                registerUser(email, senha)
             } else {
                 showBottomSheet(message = getString(R.string.password_empty_register_gragment))
             }
         }else{
             showBottomSheet(message = getString(R.string.email_empty_register_fragment))
+        }
+    }
+
+    private fun registerUser(email: String, password: String) {
+
+        try {
+            val auth = FirebaseAuth.getInstance()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        findNavController().navigate(R.id.action_global_homeFragment)
+                    } else {
+                        binding.progressbar.isVisible = false
+                    }
+                }
+        }
+        catch ( e: Exception){
+            Toast.makeText(requireContext(),e.message.toString(), Toast.LENGTH_SHORT).show()
         }
     }
 
